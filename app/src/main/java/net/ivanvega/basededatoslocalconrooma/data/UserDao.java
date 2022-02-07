4e035b3d02ffeabfc37040405085ae73afb28f18
Nahuel-Cruz <luisnahuelcruz@gmail.com>
2022-02-07 17:34:22 -0600

    Initial commit

---
+++ app/src/main/java/net/ivanvega/basededatoslocalconrooma/data/UserDao.java
@@ -0,0 +1,38 @@
+package net.ivanvega.basededatoslocalconrooma.data;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Query("select * from User")
+    public List<User> getAll();
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
+            "last_name LIKE :last LIMIT 1")
+    User findByName(String first, String last);
+
+    @Insert
+    void insertAll(User... users);
+
+    @Insert
+    long insert(User user);
+
+    @Delete
+    void delete(User user);
+
+    @Update
+    int updateUser(User user);
+    int deleteUser(User user);
+
+
+}
---
+++ app/src/main/java/net/ivanvega/basededatoslocalconrooma/provider/MiContentProvider.java
@@ -0,0 +1,180 @@
+package net.ivanvega.basededatoslocalconrooma.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.InetAddresses;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import net.ivanvega.basededatoslocalconrooma.data.AppDatabase;
+import net.ivanvega.basededatoslocalconrooma.data.User;
+import net.ivanvega.basededatoslocalconrooma.data.UserDao;
+
+import java.util.List;
+
+public class MiContentProvider extends ContentProvider {
+    /*Estructura de mi uri:
+        uri -> content://net.ivanvega.basededatoslocalconrooma.provider/user  -> insert y query
+        uri -> content://net.ivanvega.basededatoslocalconrooma.provider/user/#  -> uodate, query y delete
+        uri -> content://net.ivanvega.basededatoslocalconrooma.provider/user/*  -> query, update y delete}
+                        net.ivanvega.basededatoslocalconrooma.provider
+     */
+
+    private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static
+    {
+
+        sURIMatcher.addURI("net.ivanvega.basededatoslocalconrooma.provider","user", 1);
+        sURIMatcher.addURI("net.ivanvega.basededatoslocalconrooma.provider","user/#", 2);
+        sURIMatcher.addURI("net.ivanvega.basededatoslocalconrooma.provider","user/*", 3);
+    }
+
+    @Override
+    public boolean onCreate() {
+
+        return false;
+    }
+
+    private Cursor listUserToCursorUser( List<User>   usuaarios){
+        MatrixCursor cursor = new MatrixCursor(new String[]{
+                "uid","first_name","last_name"
+        })    ;
+
+        for(User usuario: usuaarios ){
+            cursor.newRow().add("uid", usuario.uid)
+                    .add("first_name", usuario.firstName)
+                    .add("last_name", usuario.lastName);
+        }
+
+        return cursor;
+    }
+
+
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri,
+                        @Nullable String[] strings,
+                        @Nullable String s,
+                        @Nullable String[] strings1,
+                        @Nullable String s1) {
+
+        AppDatabase db =
+                AppDatabase.getDatabaseInstance(getContext());
+
+        Cursor cursor= null;
+
+        UserDao dao = db.userDao();
+        switch (sURIMatcher.match(uri)){
+                case 1:
+                    if(TextUtils.isEmpty(s1)){
+                        cursor= listUserToCursorUser(dao.getAll());
+                    }else{
+                        List<User> listaTemporal = null;
+                        listaTemporal.add(dao.findByName(strings[0],strings[1]));
+                        cursor = listUserToCursorUser(listaTemporal);
+                    }
+                break;
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+        }
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+
+        String typeMime = "";
+
+        switch (sURIMatcher.match(uri)) {
+            case 1:
+                typeMime = "vnd.android.cursor.dir/vnd.net.ivanvega.basededatoslocalconrooma.provider.user";
+                break;
+            case 2:
+                typeMime = "vnd.android.cursor.item/vnd.net.ivanvega.basededatoslocalconrooma.provider.user";
+                break;
+            case 3:
+
+                typeMime = "vnd.android.cursor.dir/vnd.net.ivanvega.basededatoslocalconrooma.provider.user";
+                break;
+        }
+        return typeMime;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri,
+                      @Nullable ContentValues contentValues) {
+        AppDatabase db =
+                AppDatabase.getDatabaseInstance(getContext());
+        Cursor cursor= null;
+        UserDao dao = db.userDao();
+        User usuario= new User();;
+        switch (sURIMatcher.match(uri)){
+            case 1:
+
+                usuario.firstName = contentValues.getAsString(UsuarioContrato.COLUMN_FIRSTNAME);
+                usuario.lastName = contentValues.getAsString(UsuarioContrato.COLUMN_LASTNAME);
+
+                long  newid = dao.insert(usuario);
+                return  Uri.withAppendedPath(uri, String.valueOf( newid));
+
+        }
+
+        return   Uri.withAppendedPath(uri, String.valueOf( -1))  ;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        int filasAfectadas=-1;
+        switch (sURIMatcher.match(uri)){
+            case 2:
+                int idDelete = Integer.valueOf( uri.getLastPathSegment());
+                AppDatabase db =
+                        AppDatabase.getDatabaseInstance(getContext());
+                UserDao dao = db.userDao();
+                List<User>  lstUserDelete = dao.loadAllByIds(new int[]{idDelete});
+                User userDelete = lstUserDelete.get(0);
+                
+                filasAfectadas = dao.deleteUser(userDelete);
+
+                break;
+
+        }
+        return filasAfectadas;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues,
+                          @Nullable String s, @Nullable String[] strings) {
+
+         int id  = Integer.parseInt( uri.getLastPathSegment());
+
+        AppDatabase db =
+                AppDatabase.getDatabaseInstance(getContext());
+        Cursor cursor= null;
+        UserDao dao = db.userDao();
+
+        List<User> usuarioUpdate  =  dao.loadAllByIds(new int[]{id});
+
+        usuarioUpdate.get(0).firstName =
+                contentValues.getAsString(UsuarioContrato.COLUMN_FIRSTNAME );
+        usuarioUpdate.get(0).lastName =
+                contentValues.getAsString(UsuarioContrato.COLUMN_LASTNAME );
+
+        return dao.updateUser(usuarioUpdate.get(0));
+    }
+
+}
